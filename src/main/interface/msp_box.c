71fe4e542e01191a3ae9e9e317d52d36156feb17
jihlein <jihlein@hotmail.com>
2021-03-26 06:54:33 -0600

    Fix lack of tailtune mode on configurator mode page

--- src/main/interface/msp_box.c
+++ src/main/interface/msp_box.c
@@ -95,6 +95,7 @@ static const box_t boxes[CHECKBOX_ITEM_COUNT] = {
     { BOXPARALYZE, "PARALYZE", 45 },
     { BOXGPSRESCUE, "GPS RESCUE", 46 },
     { BOXNFEMODE, "NFE RACE MODE", 47 },
+    { BOXTAILTUNE, "TAILTUNE", 48 },
 };
 
 // mask of enabled IDs, calculated on startup based on enabled features. boxId_e is used as bit index
@@ -258,6 +259,7 @@ void initActiveBoxIds(void) {
     BME(BOXPIDAUDIO);
 #endif
     BME(BOXNFEMODE);
+    BME(BOXTAILTUNE);
 #undef BME
     // check that all enabled IDs are in boxes array (check may be skipped when using findBoxById() functions)
     for (boxId_e boxId = 0;  boxId < CHECKBOX_ITEM_COUNT; boxId++)
